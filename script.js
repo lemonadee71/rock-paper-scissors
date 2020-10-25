504d08c10b13a9b1b2eef3a88e92a0f9b570b233
Shin Andrei Riego <riegoshinandrei@gmail.com>
2020-10-25 17:18:27 +0800

    RPS Done

--- script.js
+++ script.js
@@ -1,83 +1,70 @@
-let choices = ['rock', 'paper', 'scissors'];
-let score = 0,
-  ties = 0;
+let computerChoices = ['rock', 'paper', 'scissors'],
+  playerChoices = {
+    65: 'rock',
+    83: 'paper',
+    68: 'scissors',
+  }
+
+let playerScore = 0,
+  computerScore = 0;
 
 const computerPlay = () => {
-  return choices[Math.floor(Math.random() * choices.length)]
+  return computerChoices[Math.floor(Math.random() * computerChoices.length)]
 }
 
 const playRound = (player, computer) => {
   switch(player) {
     case 'rock':
       if (computer === 'paper') {
-        alert('Computer plays paper. Computer wins!')
-        choices.push('scissors')
+        display('Computer plays paper. Computer wins!')
+        computerChoices.push('scissors')
+        computerScore++;
       } else if (computer === 'scissors') {
-        alert('Computer plays scissors. Player wins!')
-        choices.push('paper')
-        score++;
+        display('Computer plays scissors. Player wins!')
+        computerChoices.push('paper')
+        playerScore++;
       } else {
-        alert('We have a tie!')
-        ties++;
+        display('We have a tie!')
       }
       break;
     case 'paper':
       if (computer === 'scissors') {
-        alert('Computer plays scissors. Computer wins!')
-        choices.push('rock')
+        display('Computer plays scissors. Computer wins!')
+        computerChoices.push('rock')
+        computerScore++;
       } else if (computer === 'rock') {
-        alert('Computer plays rock. Player wins!')
-        choices.push('scissors')
-        score++;
+        display('Computer plays rock. Player wins!')
+        computerChoices.push('scissors')
+        playerScore++;
       } else {
-        alert('We have a tie!')
-        ties++;
+        display('We have a tie!')
       }
       break;
     case 'scissors':
       if (computer === 'rock') {
-        alert('Computer plays rock. Computer wins!')
-        choices.push('paper')
+        display('Computer plays rock. Computer wins!')
+        computerChoices.push('paper')
+        computerScore++;
       } else if (computer === 'scissors') {
-        alert('Computer plays paper. Player wins!')
-        choices.push('rock')
-        score++;
+        display('Computer plays paper. Player wins!')
+        computerChoices.push('rock')
+        playerScore++;
       } else {
-        alert('We have a tie!')
-        ties++;
+        display('We have a tie!')
       }
       break;
   }
 }
 
-const game = () => {
-  let rounds;
-  while (isNaN(rounds)) {
-    rounds = parseInt(prompt('How many rounds do you want to play?'))
-  }
-
-  for(let i = 0; i < rounds; i++) {
-    let playerSelection;
-    while (!playerSelection) {
-      playerSelection = prompt('Enter your choice: rock, paper or scissors').toLowerCase()
-      if (!choices.includes(playerSelection)) {
-        alert('Invalid input. Type rock, paper or scissors only.')
-        playerSelection = null;
-      }
-    }
-
-    playRound(playerSelection, computerPlay())
-  }
-
-  if (score > Math.floor((rounds - ties) / 2)) {
-    alert(`Player: ${score}, Computer: ${rounds - score - ties}. Player wins. Restart page to play again.`)
-  } else if (score < Math.floor((rounds - ties) / 2) ) {
-    alert(`Player: ${score}, Computer: ${rounds - score - ties}. Computer wins. Restart page to play again.`)
-  } else {
-    alert(`Player: ${score}, Computer: ${rounds - score - ties}. It's a tie. Restart page to play again.`)
-  }
-
-  console.log(choices)
+const display = (str) => {
+  let displayWindow = document.querySelector('#window');
+  displayWindow.textContent = str;
+  setTimeout(() => {
+    displayWindow.textContent = `Player: ${playerScore}, Computer: ${computerScore}`
+  }, 1000)
 }
-
-document.addEventListener('onload', game())
+let buttons = Array.from(document.querySelectorAll('.buttons>button'))
+buttons.forEach(btn => btn.addEventListener('click', () => {
+  playRound(playerChoices[btn.getAttribute('data-key')], computerPlay())
+}))
+window.addEventListener('keydown', (e) => playRound(playerChoices[e.keyCode], computerPlay()))
